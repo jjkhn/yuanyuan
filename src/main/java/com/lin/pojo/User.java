3df56d67686c9a51c5a0e28ed573dbf69e372d94
ly <906611193@qq.com>
2019-11-17 20:57:30 +0800

    加了

--- src/main/java/com/lin/pojo/User.java
+++ src/main/java/com/lin/pojo/User.java
@@ -4,5 +4,6 @@ public class User {
     public static void main(String[] args) {
         System.out.println("66666666");
         System.out.println("66666666");
+        System.out.println("5555");
     }
 }
